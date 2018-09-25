e8ce659597271e939797a884949cca2e4a12b7b1
Keith Jones <keithcjones21@gmail.com>
2018-09-25 14:50:27 -0600

    post route now recalls pantry

--- spoiler-alert/src/components/Form.vue
+++ spoiler-alert/src/components/Form.vue
@@ -108,8 +108,9 @@ export default {
         mode: 'cors',
       })
       .then(res => res.json())
-      .then(res => console.log(res))
-      .then(this.getFood())
+      .then(res => {
+        this.getFood()
+      })
       .then(this.showForm = false)
     },
   },
